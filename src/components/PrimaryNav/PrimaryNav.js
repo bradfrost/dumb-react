c1a822b9593dd9028148b1febb5c6b830798c6d4
Janko <jasofalcon@gmail.com>
2018-05-26 17:08:18 +0200

    Fix errors and wornings in PrimaryNav

--- src/components/PrimaryNav/PrimaryNav.js
+++ src/components/PrimaryNav/PrimaryNav.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
-import './PrimaryNav.css';
+import React, { Component } from "react";
+import "./PrimaryNav.css";
 
 export class PrimaryNav extends Component {
   render() {
-
     return (
       <nav className="c-primary-nav { this.props.styleModifier }">
         <ul className="c-primary-nav__list">
-          {this.props.listItems.map(function(listItem){
-            return <li class="c-primary-nav__item">
-              <a href={ listItem.href }>
-                { listItem.text }
-              </a>
+          {this.props.listItems.map((item, index) => (
+            <li className="c-primary-nav__item" key={index}>
+              <a href={item.href}>{item.text}</a>
             </li>
-          })}
+          ))}
         </ul>
       </nav>
     );
   }
 }
-

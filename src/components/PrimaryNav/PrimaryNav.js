db912c72db60a3701910b6bcf7955492c0401f75
Janko <jasofalcon@gmail.com>
2018-05-28 14:32:06 +0200

    Fix changes caused by autoformat of prettier

--- src/components/PrimaryNav/PrimaryNav.js
+++ src/components/PrimaryNav/PrimaryNav.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
-import "./PrimaryNav.css";
+import React, { Component } from 'react';
+import './PrimaryNav.css';
 
 export class PrimaryNav extends Component {
   render() {
+
     return (
       <nav className="c-primary-nav { this.props.styleModifier }">
         <ul className="c-primary-nav__list">
-
           {this.props.listItems.map(function(listItem, index){
             return <li class="c-primary-nav__item" key={`c-primary-nav__item-${index}`}>
               <a href={ listItem.href }>
                 { listItem.text }
               </a>
-
             </li>
           })}
         </ul>

4ac1d0a13ff722e7993cf6aca4e9bb70da43b132
Brad Frost <brad@bradfrost.com>
2018-11-01 15:28:44 -0400

    change to className

--- src/components/PrimaryNav/PrimaryNav.js
+++ src/components/PrimaryNav/PrimaryNav.js
@@ -8,7 +8,7 @@ export class PrimaryNav extends Component {
       <nav className="c-primary-nav { this.props.styleModifier }">
         <ul className="c-primary-nav__list">
           {this.props.listItems.map(function(listItem, index){
-            return <li class="c-primary-nav__item" key={`c-primary-nav__item-${index}`}>
+            return <li className="c-primary-nav__item" key={`c-primary-nav__item-${index}`}>
               <a href={ listItem.href }>
                 { listItem.text }
               </a>

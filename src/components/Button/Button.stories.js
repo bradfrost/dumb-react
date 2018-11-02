522bb49622b5004a4d5b13606560ea6654aa7800
Brad Frost <brad@bradfrost.com>
2018-11-02 09:02:10 -0400

    Add smart knobs based on proptypes

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+import { withSmartKnobs } from 'storybook-addon-smart-knobs';
 import { Button } from './Button';
 
 let stories = storiesOf('Buttons/Button', module);
 
-stories.addDecorator(withKnobs);
+stories.addDecorator(withSmartKnobs).addDecorator(withKnobs);
 
 stories.add('Default', () =>
-  <Button disabled={boolean('Disabled', false)} isSecondary={boolean('isSecondary', false)} text={text('Button Text', 'Button')} onClick={() => console.log("clicked!!")} />
+  <Button onClick={() => console.log("clicked!!")} />
 );
 
 stories.add('Secondary', () =>
